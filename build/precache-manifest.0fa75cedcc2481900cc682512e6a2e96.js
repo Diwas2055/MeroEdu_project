14080d1245401de978b3f22120cec00fe7974736
Diwas Bhandari <diwasb54@gmail.com>
2021-02-01 12:30:01 +0545

    meroedu

---
+++ build/precache-manifest.0fa75cedcc2481900cc682512e6a2e96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aad86b45207162cd188fe95920aba97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27663094ee66440fe676",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "69fd08d03553dc37c351",
+    "url": "/static/css/main.e50daf9d.chunk.css"
+  },
+  {
+    "revision": "27663094ee66440fe676",
+    "url": "/static/js/2.635f86e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.635f86e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fd08d03553dc37c351",
+    "url": "/static/js/main.3343d153.chunk.js"
+  },
+  {
+    "revision": "1edcab0189cf4eb4d4fb",
+    "url": "/static/js/runtime-main.6b8b076d.js"
+  },
+  {
+    "revision": "999afd5f8687ab2cc1730757e2f6afd6",
+    "url": "/static/media/comming soon.999afd5f.jpg"
+  },
+  {
+    "revision": "5475559a41f215514183edad18cff054",
+    "url": "/static/media/logo-footer.5475559a.png"
+  }
+]);
